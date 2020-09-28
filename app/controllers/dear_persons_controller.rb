481c837dd5f40f40949f88cba8ef2511bde0b18c
鮫島広朗 <samesame0529@gmail.com>
2020-09-28 12:55:23 +0900

    保存のための記述

--- app/controllers/dear_persons_controller.rb
+++ app/controllers/dear_persons_controller.rb
@@ -1,2 +1,22 @@
 class DearPersonsController < ApplicationController
+  def new
+    @dear_person = DearPerson.new  
+  end
+
+  def create
+    @dear_person = DearPerson.new(dear_person_params)
+    if @dear_person.valid?
+      
+      @dear_person.save  # バリデーションをクリアした時 
+      redirect_to  dear_person_path(@dear_person.id) 
+    else
+      ender 'new' # バリデーションに弾かれた時
+    end
+  end
+
+  private
+
+  def dear_person_params
+    params.require(:dear_person).permit(:image, :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, :anniversary,).merge(user_id: current_user.id)
+  end
 end
--- app/models/dear_person.rb
+++ app/models/dear_person.rb
@@ -1,4 +1,6 @@
 class DearPerson < ApplicationRecord
 
   belongs_to :user  # usersテーブルとのアソシエーション
+
+  validates :text, presence: true # 空だと登録できないバリデーション
 end
