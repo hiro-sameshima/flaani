65167e57bf86b9f36e9ca6677d435f8873b12b45
鮫島広朗 <samesame0529@gmail.com>
2020-09-28 13:32:44 +0900

    dear_person保存とshowのview

--- app/controllers/dear_persons_controller.rb
+++ app/controllers/dear_persons_controller.rb
@@ -5,18 +5,22 @@ class DearPersonsController < ApplicationController
 
   def create
     @dear_person = DearPerson.new(dear_person_params)
+    binding.pry
     if @dear_person.valid?
       
       @dear_person.save  # バリデーションをクリアした時 
       redirect_to  dear_person_path(@dear_person.id) 
     else
-      ender 'new' # バリデーションに弾かれた時
+      render 'new' # バリデーションに弾かれた時
     end
   end
 
+  def show
+    @dear_person = DearPerson.find(params[:id])
+  end
   private
 
   def dear_person_params
-    params.require(:dear_person).permit(:image, :nickname, :family_name, :first_name, :family_name_kana, :first_name_kana, :birthday, :anniversary,).merge(user_id: current_user.id)
+    params.require(:dear_person).permit(:image, :nick_name, :family_name, :last_name, :family_name_kana, :last_name_kana, :birthday, :anniversary,).merge(user_id: current_user.id)
   end
 end
--- app/models/dear_person.rb
+++ app/models/dear_person.rb
@@ -1,6 +1,14 @@
 class DearPerson < ApplicationRecord
 
   belongs_to :user  # usersテーブルとのアソシエーション
+  has_one_attached :image # 各画像と1対1で結びつける
 
-  validates :text, presence: true # 空だと登録できないバリデーション
+   # 空だと登録できないバリデーション
+  with_options presence: true do
+    validates :nick_name
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+  end
 end
