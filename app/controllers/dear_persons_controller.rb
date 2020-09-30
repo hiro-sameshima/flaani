bad91acf3bcaa5c74ad0e692dc41df7db57cad66
鮫島広朗 <samesame0529@gmail.com>
2020-09-30 17:41:04 +0900

    コード整形

--- app/controllers/dear_persons_controller.rb
+++ app/controllers/dear_persons_controller.rb
@@ -1,20 +1,19 @@
 class DearPersonsController < ApplicationController
-
   def index
     @dear_persons = DearPerson.all
     @user = User.find(current_user[:id])
   end
 
   def new
-    @dear_person = DearPerson.new  
+    @dear_person = DearPerson.new
   end
 
   def create
     @dear_person = DearPerson.new(dear_person_params)
     if @dear_person.valid?
-      
-      @dear_person.save  # バリデーションをクリアした時 
-      redirect_to  dear_person_path(@dear_person.id) 
+
+      @dear_person.save  # バリデーションをクリアした時
+      redirect_to  dear_person_path(@dear_person.id)
     else
       render 'new' # バリデーションに弾かれた時
     end
@@ -40,11 +39,12 @@ class DearPersonsController < ApplicationController
   def destroy
     dear_person = DearPerson.find(params[:id])
     dear_person.destroy
-      redirect_to root_path
-end
+    redirect_to root_path
+  end
+
   private
 
   def dear_person_params
-    params.require(:dear_person).permit(:image, :nick_name, :family_name, :last_name, :family_name_kana, :last_name_kana, :birthday, :anniversary,).merge(user_id: current_user.id)
+    params.require(:dear_person).permit(:image, :nick_name, :family_name, :last_name, :family_name_kana, :last_name_kana, :birthday, :anniversary).merge(user_id: current_user.id)
   end
 end
--- app/models/dear_person.rb
+++ app/models/dear_person.rb
@@ -1,9 +1,8 @@
 class DearPerson < ApplicationRecord
-
   belongs_to :user  # usersテーブルとのアソシエーション
   has_one_attached :image # 各画像と1対1で結びつける
 
-   # 空だと登録できないバリデーション
+  # 空だと登録できないバリデーション
   with_options presence: true do
     validates :nick_name
     validates :image
