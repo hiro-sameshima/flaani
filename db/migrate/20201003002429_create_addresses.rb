a6ad723198af423fa0ae94e71ce5bef4b79f16a2
鮫島広朗 <samesame0529@gmail.com>
2020-10-03 09:58:09 +0900

    住所テーブルのマイグレーションファイル記述

--- db/migrate/20201003002429_create_addresses.rb
+++ db/migrate/20201003002429_create_addresses.rb
@@ -1,7 +1,14 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-
+      t.string :postal_code,           null: false
+      t.string :city,                  null: false
+      t.string :house_number,          null: false
+      t.string :building_name,         null: false
+      t.string :phone_number,          null: false
+      t.integer :prefecture_id,        null: false
+      t.integer :delivery_time_id,     null: false
+      t.references :dear_person,       foreign_key: true, null: false
       t.timestamps
     end
   end
