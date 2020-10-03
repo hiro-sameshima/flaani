d01100bf698617c421fbf808c7f724e96fdb76b4
鮫島広朗 <samesame0529@gmail.com>
2020-10-03 10:09:54 +0900

    各テーブル作成

--- db/migrate/20201003002458_create_payments.rb
+++ db/migrate/20201003002458_create_payments.rb
@@ -1,8 +1,8 @@
 class CreatePayments < ActiveRecord::Migration[6.0]
   def change
     create_table :payments do |t|
-      t.references :price,                null: false,  foreign_key: true
-      t.references :user,                 null: false,  foreign_key: true
+      t.integer :price,           null: false
+      t.references :user,         null: false,  foreign_key: true
       t.timestamps
     end
   end
