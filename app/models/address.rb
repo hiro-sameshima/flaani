1ee25b25ae58bae43dcef98c6e140f21a207e180
鮫島広朗 <samesame0529@gmail.com>
2020-10-03 10:38:28 +0900

    アクティブハッシュを使用する際のアソシエーション

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,8 @@
 class Address < ApplicationRecord
+  # アクティブハッシュを使用する際のアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :delivery_time
+
   belongs_to :plan
 end
--- app/models/delivery_time.rb
+++ app/models/delivery_time.rb
@@ -1,2 +1,11 @@
 class DeliveryTime < ApplicationRecord
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '午前中（8時～12時' },
+    { id: 3, name: '14時～16時' },
+    { id: 4, name: '16時～18時' },
+    { id: 5, name: '18時～20時' },
+    { id: 6, name: '19時～21時' },
+    { id: 7, name: '指定なし' },
+  ]
 end
