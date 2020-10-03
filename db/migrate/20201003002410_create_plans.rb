2750619bb6076dbec37ac6b3340e9725d6b6c748
鮫島広朗 <samesame0529@gmail.com>
2020-10-03 09:52:56 +0900

    プランのマイグレーションファイル記述

--- db/migrate/20201003002410_create_plans.rb
+++ db/migrate/20201003002410_create_plans.rb
@@ -1,7 +1,10 @@
 class CreatePlans < ActiveRecord::Migration[6.0]
   def change
     create_table :plans do |t|
-
+      # テーブルの記述から
+      t.integer :price,           null: false
+      t.string :style,            null: false
+      t.references :dear_person,  null: false,  foreign_key: true
       t.timestamps
     end
   end
