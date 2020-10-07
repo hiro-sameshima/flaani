5163bfe83acdbf80c720e31589f04a0aa6848fbb
鮫島広朗 <samesame0529@gmail.com>
2020-10-07 09:22:04 +0900

    修正

---
+++ test/controllers/florists_controller_test.rb
@@ -0,0 +1,9 @@
+require 'test_helper'
+
+class FloristsControllerTest < ActionDispatch::IntegrationTest
+  test "should get show" do
+    get florists_show_url
+    assert_response :success
+  end
+
+end
