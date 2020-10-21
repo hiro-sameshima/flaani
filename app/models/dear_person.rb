ca1bee54542618bbfb4270f01142fb9e39d3e268
鮫島広朗 <samesame0529@gmail.com>
2020-10-21 11:48:39 +0900

    DearPerson機能の単体テストコード

--- app/models/dear_person.rb
+++ app/models/dear_person.rb
@@ -7,9 +7,12 @@ class DearPerson < ApplicationRecord
   with_options presence: true do
     validates :nick_name
     validates :image
+    validates :birthday
+    validates :anniversary
     validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width katakana characters.' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: 'is invalid. Input full-width katakana characters.' }
     validates :family_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+
   end
 end
