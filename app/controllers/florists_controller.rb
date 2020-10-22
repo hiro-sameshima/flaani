3464e864d05b8baed838b857b26261b4424c36f1
鮫島広朗 <samesame0529@gmail.com>
2020-10-22 11:39:21 +0900

    コード修正

--- app/controllers/florists_controller.rb
+++ app/controllers/florists_controller.rb
@@ -1,8 +1,14 @@
 class FloristsController < ApplicationController
   def show
-    @persons = DearPerson.all
     @florist = current_florist
     @plans = Plan.all
     @addresses = Address.all
+    @plan_address_person = []
+    @persons = DearPerson.all
+    @persons.each do |person|
+      unless person.plan.nil?
+        @plan_address_person.push(person)
+      end
+    end
   end
 end
