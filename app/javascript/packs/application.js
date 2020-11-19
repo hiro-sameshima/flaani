e1e5f5c8152c1940c12966c40916aba8a8fed00e
鮫島広朗 <samesame0529@gmail.com>
2020-11-19 12:26:57 +0900

    修正③

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,16 +2,13 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//= require jquery
-//= require jquery_ujs
-// require("turbolinks").start()
-//= require_tree . #require_treeより上にjquery、jquery_ujsを書くこと
-//= require activestorage
-require("@rails/ujs").start()
 
+require("@rails/ujs").start()
+// require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../card") 
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
